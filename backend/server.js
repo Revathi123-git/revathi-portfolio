0764d0c7310450e45414b3eae60a808091ebe7c9
Revathi123-git <revathiparisarla@gamil.com>
2025-05-23 15:56:14 +0530

    Added root route to server.js

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,16 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", 
+    "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;");
+  next();
+});
 app.use('/api/contact', contactRoutes); 
 
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
